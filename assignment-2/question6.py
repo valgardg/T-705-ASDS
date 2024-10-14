09e338052cb2af811e83e88065fb387a6c5d94bd
valgardg <valgardo20@ru.is>
2024-10-13 22:59:00 -0400

    completed a.

--- assignment-2/question6.py
+++ assignment-2/question6.py
@@ -1,14 +1,24 @@
 import pandas as pd
 
+def find_lowest_median(df):
+    numeric_cols = df.select_dtypes(include=['number'])
+    medians = numeric_cols.median()
+    
+    min_median_column = medians.idxmin()
+    min_median_value = medians.min()
+    
+    return min_median_column, min_median_value
+
 def read_csv():
     file_path = 'Credit.csv'
     df = pd.read_csv(file_path)
-
-    print(df)
+    return df
 
 
 def main():
-    read_csv()
+    df = read_csv()
+    min_var, min_value = find_lowest_median(df)
+    print(f'var with lowest median value: {min_var}, value: {min_value}')
 
 if __name__ == "__main__":
     main()
