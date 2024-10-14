5dbfa975526a8a44acd639dc51c2964bb9254d92
valgardg <valgardo20@ru.is>
2024-10-13 23:02:25 -0400

    completed b.

--- assignment-2/question6.py
+++ assignment-2/question6.py
@@ -9,16 +9,28 @@ def find_lowest_median(df):
     
     return min_median_column, min_median_value
 
+def find_greatest_range(df):
+    numeric_cols = df.select_dtypes(include=['number'])
+    
+    ranges = numeric_cols.max() - numeric_cols.min()
+    
+    max_range_column = ranges.idxmax()
+    max_range_value = ranges.max()
+    
+    return max_range_column, max_range_value
+
 def read_csv():
     file_path = 'Credit.csv'
     df = pd.read_csv(file_path)
     return df
 
-
 def main():
     df = read_csv()
     min_var, min_value = find_lowest_median(df)
     print(f'var with lowest median value: {min_var}, value: {min_value}')
+
+    max_range_var, max_value = find_greatest_range(df)
+    print(f'var with greatest range: {max_range_var}, value: {max_value}')
 
 if __name__ == "__main__":
     main()
